7b85634fe578a0a10874b5a0c2c1d1dddab163fe
gomez7373 <sgc.holberton@gmail.com>
2024-07-16 22:09:41 -0400

    files

---
+++ ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+// File: 4-main.js
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));                   // Output: 1
+console.log(returnHowManyArguments("one", "two", 3, "4th"));  // Output: 4
