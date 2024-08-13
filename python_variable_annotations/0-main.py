d7c3c99863928ff2b78a1d901a1401aa48e4b53d
gomez7373 <sgc.holberton@gmail.com>
2024-08-13 19:51:27 -0400

    files

--- python_variable_annotations/0-main.py
+++ python_variable_annotations/0-main.py
@@ -1,11 +1,5 @@
-"""
-Module for adding two floats together.
-
-This module provides a function to add two
-floating-point numbers and return their sum.
-"""
+#!/usr/bin/env python3
 add = __import__('0-add').add
 
 print(add(1.11, 2.22) == 1.11 + 2.22)
 print(add.__annotations__)
-
