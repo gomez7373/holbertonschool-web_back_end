3a55669ae37622bf97b36477652513c1055be590
gomez7373 <sgc.holberton@gmail.com>
2024-08-28 21:38:05 -0400

    dic nad files

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" this is the last function of the day """
+""" log stats """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """Last function of the day"""
+    """ main """
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
