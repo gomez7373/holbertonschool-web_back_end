03a6871294834324e78491babeaa4cbf6557d6e1
gomez7373 <sgc.holberton@gmail.com>
2024-08-28 21:16:12 -0400

    dic nad files

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" pymongo list """
-
-import pymongo
-
+from pymongo import MongoClient
 
 def list_all(mongo_collection):
-    """ it will list all elements in a collection """
-    if not mongo_collection:
+    """
+    Lists all documents in a collection
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The collection object to list documents from.
+
+    Returns:
+        list: A list of all documents in the collection. Returns an empty list if no documents are found.
+    """
+    if mongo_collection is None:
         return []
+    
     return list(mongo_collection.find())
