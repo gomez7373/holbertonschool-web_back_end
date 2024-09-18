6f085115f6e6a7c43699b2d815c2031f688250db
gomez7373 <sgc.holberton@gmail.com>
2024-09-17 20:32:21 -0400

    file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,13 +3,13 @@ console.log("Welcome to Holberton School, what is your name?");
 
 // Capture user input
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();  // Remove any extra spaces or newlines
-    console.log(`Your name is: ${name}`);
-    process.exit();
+  const name = data.toString().trim();  // Remove any extra spaces or newlines
+  console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
 // Display message when process exits
 process.on('exit', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
 
