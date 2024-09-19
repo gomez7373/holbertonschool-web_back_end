dd50668cc495d753fca51fe7963111abee76da41
gomez7373 <sgc.holberton@gmail.com>
2024-09-18 23:53:40 -0400

    files

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,11 @@
+/**
+ * Application controller
+ */
 class AppController {
-    static getHomepage(req, res) {
-      res.status(200).send('Hello Holberton School!');
+    static getHomepage(request, response) {
+      response.statusCode = 200;
+      response.send('Hello Holberton School!');
     }
   }
   
   export default AppController;
-  
--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,45 +1,49 @@
-import readDatabase from '../utils.js';
+/**
+ * Student controller class
+ */
+import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents(req, res) {
-    const databasePath = process.argv[2];
-
-    readDatabase(databasePath)
-      .then((fields) => {
-        let responseText = 'This is the list of our students\n';
-
-        // Sort fields alphabetically and case-insensitive
-        Object.keys(fields).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())).forEach((field) => {
-          responseText += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-        });
-
-        res.status(200).send(responseText.trim()); // Use trim() to remove the last newline
-      })
-      .catch((error) => {
-        res.status(500).send('Cannot load the database');
+  static async getAllStudents(request, response) {
+    try {
+      const filepath = process.argv.length > 2 ? process.argv[2] : '';
+      const records = await readDatabase(filepath);
+      const fields = Object.keys(records);
+      fields.sort((x, y) => {
+        if (x < y) return -1;
+        if (x > y) return 1;
+        return 0;
       });
+      response.statusCode = 200;
+      response.write('This is the list of our students\n');
+      for (const field of fields) {
+        response.write(`Number of students in ${field}: ${records[field].length}. List: ${records[field].join(', ')}`);
+        if (fields.indexOf(field) !== fields.length - 1) response.write('\n');
+      }
+    } catch (err) {
+      response.statusCode = 500;
+      response.write(err.message);
+    }
+    response.end();
   }
 
-  static getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
-    const databasePath = process.argv[2];
-
-    if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).send('Major parameter must be CS or SWE');
-      return;
+  static async getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+    if (['CS', 'SWE'].indexOf(major.toUpperCase()) === -1) {
+      response.statusCode = 500;
+      response.send('Major parameter must be CS or SWE');
+    } else {
+      const filepath = process.argv.length > 2 ? process.argv[2] : '';
+      try {
+        const records = await readDatabase(filepath);
+        response.statusCode = 200;
+        response.write(`List: ${records[major.toUpperCase()].join(', ')}`);
+      } catch (err) {
+        response.statusCode = 500;
+        response.write(err.message);
+      }
+      response.end();
     }
-
-    readDatabase(databasePath)
-      .then((fields) => {
-        if (!fields[major]) {
-          res.status(500).send('Major parameter must be CS or SWE');
-        } else {
-          res.status(200).send(`List: ${fields[major].join(', ')}`);
-        }
-      })
-      .catch(() => {
-        res.status(500).send('Cannot load the database');
-      });
   }
 }
 
