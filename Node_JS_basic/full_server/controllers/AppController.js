3e9870942ed1da2214452b34415d5a1b4084da2b
gomez7373 <sgc.holberton@gmail.com>
2024-09-19 21:51:32 -0400

    file

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -2,10 +2,10 @@
  * Application controller
  */
 class AppController {
-    static getHomepage(request, response) {
-      response.statusCode = 200;
-      response.send('Hello Holberton School!');
-    }
+  static getHomepage(request, response) {
+    response.statusCode = 200;
+    response.send('Hello Holberton School!');
   }
-  
-  export default AppController;
+}
+
+export default AppController;
